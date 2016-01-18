093cb655583b5e04c1cfbf73491d39c38448481f
Javen205 <javen205@126.com>
2016-01-18 22:00:43 +0800

    jfinal weixin demo 1.7 release ^_^

--- src/com/jfinal/weixin/demo/WeixinConfig.java
+++ src/com/jfinal/weixin/demo/WeixinConfig.java
@@ -1,11 +1,9 @@
 /**
- * Copyright (c) 2011-2014, James Zhan 詹波 (jfinal@126.com).
+  * Copyright (c) 2015-2016, Javen Zhou  (javenlife@126.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
-
 package com.jfinal.weixin.demo;
-
 import com.jfinal.config.Constants;
 import com.jfinal.config.Handlers;
 import com.jfinal.config.Interceptors;
@@ -14,8 +12,15 @@ import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
 import com.jfinal.core.JFinal;
 import com.jfinal.kit.PropKit;
+import com.jfinal.render.ViewType;
 import com.jfinal.weixin.sdk.api.ApiConfigKit;
-
+import com.jfinal.weixin.share.ShareController;
+import com.jfinal.weixin.usercontroller.UserController;
+import com.jfinal.weixin.weboauth2.RedirectUri;
+/**
+ * @author Javen
+ * 2016年1月15日
+ */
 public class WeixinConfig extends JFinalConfig {
 	
 	/**
@@ -35,7 +40,8 @@ public class WeixinConfig extends JFinalConfig {
 	public void configConstant(Constants me) {
 		loadProp("a_little_config_pro.txt", "a_little_config.txt");
 		me.setDevMode(PropKit.getBoolean("devMode", false));
-		
+		me.setEncoding("utf-8");
+		me.setViewType(ViewType.JSP);
 		// ApiConfigKit 设为开发模式可以在开发阶段输出请求交互的 xml 与 json 数据
 		ApiConfigKit.setDevMode(me.getDevMode());
 	}
@@ -43,6 +49,12 @@ public class WeixinConfig extends JFinalConfig {
 	public void configRoute(Routes me) {
 		me.add("/msg", WeixinMsgController.class);
 		me.add("/api", WeixinApiController.class, "/api");
+		me.add("/pay", WeixinPayController.class);
+		me.add("/", IndexController.class,"/index");
+		me.add("/oauth2",RedirectUri.class);
+		me.add("/user",UserController.class);
+		me.add("/jssdk",ShareController.class,"_front");
+
 	}
 	
 	public void configPlugin(Plugins me) {
@@ -51,6 +63,9 @@ public class WeixinConfig extends JFinalConfig {
 		
 		// EhCachePlugin ecp = new EhCachePlugin();
 		// me.add(ecp);
+		
+		// RedisPlugin redisPlugin = new RedisPlugin("weixin", "127.0.0.1");
+		// me.add(redisPlugin);
 	}
 	
 	public void configInterceptor(Interceptors me) {
@@ -61,7 +76,16 @@ public class WeixinConfig extends JFinalConfig {
 		
 	}
 	
-	public static void main(String[] args) {
-		JFinal.start("webapp", 80, "/", 5);
+	public void afterJFinalStart() {
+		// 1.5 之后支持redis存储access_token、js_ticket，需要先启动RedisPlugin
+//		ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache());
+		// 1.6新增的2种初始化
+//		ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache(Redis.use("weixin")));
+//		ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache("weixin"));
 	}
+
+	public static void main(String[] args) {
+		JFinal.start("webapp", 8080, "/", 5);
+	}
+	
 }

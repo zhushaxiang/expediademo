16a7bb1e50e56f02ce515c8e50e9a81b53dcc00e
Banson Liu <bliu@SEA.CORP.EXPECN.com>
2016-11-15 16:44:48 +0800

    Update

--- src/main/java/com/jfinal/weixin/demo/IndexController.java
+++ src/main/java/com/jfinal/weixin/demo/IndexController.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2015-2016, Javen Zhou  (javenlife@126.com).
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
 
@@ -10,11 +10,13 @@ import com.jfinal.core.Controller;
 
 /**
  * @author Javen
- * 2016年1月15日
+ *         2016年1月15日
  */
-public class IndexController extends Controller {
-	public void index(){
-		setAttr("test", "这里是测试...");
-		render("index.jsp");
-	}
+public class IndexController extends Controller
+{
+    public void index()
+    {
+        setAttr("Expedia", "这里是测试...");
+        render("index.jsp");
+    }
 }
